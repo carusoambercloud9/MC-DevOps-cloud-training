6e3c1d75601c406ab02ae45dbc6ebd5312b5f851
Amber Caruso <carusoamber80@gmail.com>
2026-05-19 17:57:35 -0600

    feat: Add ECS auto-scaling policy for Assessment 19-20

--- Dev/ecs-service.tf
+++ Dev/ecs-service.tf
@@ -56,3 +56,28 @@ resource "aws_ecs_service" "mc_app" {
     Project     = "MaidCentral"
   }
 }
+# Auto Scaling for ECS Service
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.mc_cluster.name}/${aws_ecs_service.mc_app.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_cpu_policy" {
+  name               = "mc-dev-cpu-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 70.0
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+  }
+}
